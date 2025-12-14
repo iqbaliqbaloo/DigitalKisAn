14bf321393baa5ac33bf03a8284b252040491a80
Muhammad Iqbal <iqbaliqbaloolife@gmail.com>
2025-12-14 12:05:13 +0500

    Remove backend folder from GitHub

--- backend/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProducts, addProduct } from "../controllers/product.controller.js";
-
-const router = express.Router();
-
-router.get("/", getProducts);
-router.post("/", addProduct);
-
-export default router;
